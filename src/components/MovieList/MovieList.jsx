3971d4176fb8ea84739ca9d4577254b86642f532
Laney McKee <laneymckeephotography@gmail.com>
2019-08-30 18:07:01 -0500

    completed dispatch calls on client side to complete get route to dom, movies displayed in table

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,16 +2,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+//COMPONENT FOR MOVIE LIST ITEMS IN TABLE
 import MovieItem from '../MovieItem/MovieItem';
 
 class MovieList extends Component {
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  getMovies() {
+    this.props.dispatch({
+      type: 'FETCH_MOVIES'
+    });
+  }
+
   render() {
+    //for every movie in our movieReducer [] in redux
+    //create a movieItem to be rendered to table in dom
+    let movieList = this.props.movies.map(movie => {
+      return <MovieItem key={movie.id} movie={movie} />;
+    });
     return (
       <div>
-        <MovieItem />
+        <table>
+          <tbody>{movieList}</tbody>
+        </table>
       </div>
     );
   }
 }
 
-export default connect()(MovieList);
+const mapStateToProps = reduxStore => ({
+  movies: reduxStore.movies
+});
+
+export default connect(mapStateToProps)(MovieList);
