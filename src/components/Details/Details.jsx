f439c9d269eff5eb877cb4eb26076fb76888a06e
Laney McKee <laneymckeephotography@gmail.com>
2019-08-30 20:40:29 -0500

    finished setup of basic content in pages, with outlined event functions

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,15 +1,26 @@
 //REACT IMPORTS
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 class Details extends Component {
+returnHome = (event) => {
+    this.props.history.push('/')
+}
+
+editMovie = (event) => {
+    this.props.history.push('/edit')
+}
+
   render() {
     return (
       <div>
-        <p>Empty Page</p>
+        <p>DETAILS</p>
+        <button onClick={this.returnHome}>Back To List</button>
+        <button onClick={this.editMovie}>Edit</button>
       </div>
     );
   }
 }
 
-export default connect()(Details);
+export default withRouter(connect()(Details));
--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,13 +1,21 @@
 //REACT IMPORTS
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 class MovieItem extends Component {
+  seeDetails = event => {
+    this.props.history.push('/details');
+  };
+
   render() {
     return (
       <tr>
         <td>
-          <img alt={this.props.movie.title} src={this.props.movie.poster} />
+          <img
+            alt={this.props.movie.title}
+            src={this.props.movie.poster}
+            onClick={this.seeDetails}
+          />
         </td>
         <td>
           <h2>{this.props.movie.title}</h2>
@@ -19,4 +27,4 @@ class MovieItem extends Component {
   }
 }
 
-export default connect()(MovieItem);
+export default withRouter(MovieItem);
